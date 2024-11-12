c68fde4aba69804915166c9a91acd63879551afe
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2024-11-12 15:41:43 +0100

    cards as links done

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,14 +1,17 @@
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const EventCard = ({ cover, title, description }) => {
+const EventCard = ({ cover, title, description, id }) => {
 
     return (
         <div className="EventCard">
             <Card className="bg-dark text-white">
                 <Card.Img src={cover} alt="Card image" />
-                <Card.ImgOverlay className="d-flex justify-content-center align-items-start">
-                    <Card.Title className="text-center w-100">{title}</Card.Title>
-                </Card.ImgOverlay>
+                <Link to={`/eventos/detalles/${id}`} style={{ textDecoration: 'none' }}>
+                    <Card.ImgOverlay className="d-flex justify-content-center align-items-start">
+                        <Card.Title className="event-title text-center w-100">{title}</Card.Title >
+                    </Card.ImgOverlay>
+                </Link>
             </Card>
         </div>
     );
