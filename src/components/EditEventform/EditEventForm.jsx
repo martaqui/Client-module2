be5e0733faf5c1f465c2296ea3771b730b905e03
john sebastian marmolejo castro <148784346+Lechattarre@users.noreply.github.com>
2024-11-14 16:11:13 +0100

    Crud Event finish

--- src/components/EditEventform/EditEventForm.jsx
+++ src/components/EditEventform/EditEventForm.jsx
@@ -1,7 +1,239 @@
-const EditEventForm = () => {
-    return (
-        <h1>hola</h1>
-    )
-}
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import './EditEventForm.css';
 
-export default EditEventForm
+const API_URL = "http://localhost:5005";
+x
+const EditEventForm = ({ id }) => {
+    const navigate = useNavigate();
+
+    const [eventData, setEventData] = useState({
+        title: '',
+        genres: '',
+        capacity: 0,
+        date: '',
+        vip: false,
+        description: '',
+        minAge: '',
+        available: true,
+    });
+
+    const [location, setLocation] = useState({
+        street: '',
+        city: '',
+        zipcode: ''
+    });
+
+    const [price, setPrice] = useState({
+        regular: 0,
+        early: 0
+    });
+
+    useEffect(() => {
+        axios
+            .get(`${API_URL}/events/${id}`)
+            .then(response => {
+                const event = response.data;
+                setEventData({
+                    title: event.title,
+                    genres: event.genres,
+                    capacity: event.capacity,
+                    date: event.date,
+                    vip: event.vip,
+                    description: event.description,
+                    minAge: event.minAge,
+                    available: event.available,
+                });
+
+                setLocation({
+                    street: event.location.street,
+                    city: event.location.city,
+                    zipcode: event.location.zipcode
+                });
+
+                setPrice({
+                    regular: event.price.regular,
+                    early: event.price.early
+                });
+            })
+            .catch(error => console.error("Error al cargar el evento:", error));
+    }, [id]);
+
+    const handleLocationChange = e => {
+        const { name, value } = e.target;
+        setLocation({ ...location, [name]: value });
+    };
+
+    const handlePriceChange = e => {
+        const { name, value } = e.target;
+        setPrice({ ...price, [name]: value });
+    };
+
+    const handleEventChange = e => {
+        const { name, value } = e.target;
+        setEventData({ ...eventData, [name]: value });
+    };
+
+    const handleEventSubmit = (e) => {
+        e.preventDefault();
+
+        const reqPayload = {
+            ...eventData,
+            location: { ...location },
+            price: { ...price },
+            cover: "https://cdn.pixabay.com/photo/2019/10/15/03/16/black-and-white-4550471_960_720.jpg"
+        };
+
+        axios
+            .put(`${API_URL}/events/${id}`, reqPayload)
+            .then(response => {
+                navigate(`/eventos/detalles/${response.data.id}`);
+            })
+            .catch(error => {
+                console.error("Error al actualizar el evento:", error);
+            });
+    };
+
+    return (
+        <div className="EditEventForm">
+            <Container>
+                <Form onSubmit={handleEventSubmit}>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} xs={8} controlId="formEventName">
+                            <Form.Label>Nombre del evento</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={eventData.title}
+                                onChange={handleEventChange}
+                                name="title"
+                            />
+                        </Form.Group>
+
+                        <Form.Group as={Col} xs={2} controlId="formEventCapacity">
+                            <Form.Label>Aforo</Form.Label>
+                            <Form.Control
+                                type="number"
+                                min={1}
+                                value={eventData.capacity}
+                                onChange={handleEventChange}
+                                name="capacity"
+                            />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} xs={2} controlId="formEventPriceRegular">
+                            <Form.Label>Precio Regular</Form.Label>
+                            <Form.Control
+                                type="number"
+                                value={price.regular}
+                                onChange={handlePriceChange}
+                                name="regular"
+                            />
+                        </Form.Group>
+
+                        <Form.Group as={Col} xs={2} controlId="formEventPriceEarly">
+                            <Form.Label>Precio Anticipado</Form.Label>
+                            <Form.Control
+                                type="number"
+                                value={price.early}
+                                onChange={handlePriceChange}
+                                name="early"
+                            />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} xs={8} controlId="formEventAgeRequirement">
+                            <Form.Label>Requerimiento de Edad</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Edad mínima (e.g., 18)"
+                                value={eventData.minAge}
+                                onChange={handleEventChange}
+                                name="minAge"
+                            />
+                        </Form.Group>
+
+                        <Form.Group as={Col} xs={4} controlId="formEventVipReserved">
+                            <Form.Label>Reservados VIP</Form.Label>
+                            <Form.Select
+                                name="vip"
+                                value={eventData.vip ? "Disponible" : "No Disponible"}
+                                onChange={(e) =>
+                                    setEventData({ ...eventData, vip: e.target.value === "Disponible" })
+                                }
+                            >
+                                <option value="No Disponible">No Disponible</option>
+                                <option value="Disponible">Disponible</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3">
+                        <Form.Group controlId="formEventDescription">
+                            <Form.Label>Descripción</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                value={eventData.description}
+                                onChange={handleEventChange}
+                                name="description"
+                            />
+                        </Form.Group>
+                    </Row>
+
+                    <Form.Group className="mb-3" controlId="formEventLocation">
+                        <Form.Label>Dirección del Evento</Form.Label>
+                        <Form.Control
+                            placeholder="1234 Calle Principal"
+                            value={location.street}
+                            onChange={handleLocationChange}
+                            name="street"
+                        />
+                    </Form.Group>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formEventDate">
+                            <Form.Label>Fecha</Form.Label>
+                            <Form.Control
+                                type="date"
+                                value={eventData.date}
+                                onChange={handleEventChange}
+                                name="date"
+                            />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formEventCity">
+                            <Form.Label>Ciudad</Form.Label>
+                            <Form.Control
+                                value={location.city}
+                                onChange={handleLocationChange}
+                                name="city"
+                            />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formEventZip">
+                            <Form.Label>Código Postal</Form.Label>
+                            <Form.Control
+                                value={location.zipcode}
+                                onChange={handleLocationChange}
+                                name="zipcode"
+                            />
+                        </Form.Group>
+                    </Row>
+
+                    <Col>
+                        <Button variant="primary" type="submit">
+                            Actualizar Evento
+                        </Button>
+                    </Col>
+                </Form>
+            </Container>
+        </div>
+    );
+};
+
+export default EditEventForm;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,27 +1,39 @@
 import "./Navigation.css"
 import iconImg from "./../../assets/img/iconPag2.png"
 import { Link } from "react-router-dom"
-import { Form } from "react-bootstrap"
+import { Form, Container, Row, Col } from "react-bootstrap"
+import { RxPerson } from "react-icons/rx";
+
 const Navigation = () => {
     return (
         <div className="Navigation">
-            <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+            <Container>
+                <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
 
-                <div className="container-fluid justify-content-between">
-                    <Link to={"/"}>
-                        <img src={iconImg} alt="iconImage" className="navbar-icon" />
-                    </Link>
-                    <Form.Control
-                        type="search"
-                        placeholder="Search"
-                        className="navbar-search me-2"
-                        aria-label="Search"
-                    />
-                    <Link to={"/"}><li>Home</li></Link>
-                    <Link to={"/eventos"}><li>Events</li></Link>
-                    <Link to={"/Edit/Event/Form"}><li>Profile</li></Link>
-                </div>
-            </nav >
+                    <div className="container-fluid justify-content-between">
+                        <Link to={"/"}>
+                            <img src={iconImg} alt="iconImage" className="navbar-icon" />
+                        </Link>
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="navbar-search me-2"
+                            aria-label="Search"
+                        />
+                        <Row>
+                            <Col xs="5">
+                                <Link to={"/"}><li>Home</li></Link>
+                            </Col>
+                            <Col xs="5">
+                                <Link to={"/eventos"}><li>Events</li></Link>
+                            </Col>
+                            <Col xs="10">
+                                <Link to={"/"}><li><RxPerson /></li></Link>
+                            </Col>
+                        </Row>
+                    </div>
+                </nav >
+            </Container>
         </div >
     )
 }
