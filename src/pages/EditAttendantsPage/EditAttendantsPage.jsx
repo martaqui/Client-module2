9637bbe331c7fdc2a69e3b25f11c5f6025c7ec04
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2024-11-14 11:43:03 +0100

    eddit attendants page

---
+++ src/pages/EditAttendantsPage/EditAttendantsPage.jsx
@@ -0,0 +1,8 @@
+const EditAttendantsPage = () => {
+    return (
+        <div className="EditAttendantsPage">
+            <h1>hollloooo</h1>
+        </div>
+    )
+}
+export default EditAttendantsPage
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,6 +6,7 @@ import ProfilePage from "../pages/ProfilePage/ProfilePage"
 import EventDetailsPage from "../pages/EventDetailsPage/EventDetailsPage"
 import FormUserPage from "../pages/FormUser/FormUserPage"
 import CreateEventPage from "../pages/CreateEventsPage/CreateEventsPage"
+import EditAttendantsPage from "../pages/EditAttendantsPage/EditAttendantsPage"
 
 const AppRoutes = () => {
     return (
@@ -16,6 +17,7 @@ const AppRoutes = () => {
             <Route path="/perfil" element={<ProfilePage />} />
             <Route path="/evento/:eventId/registro" element={<FormUserPage />} />
             <Route path="/Create/Event/Form" element={<CreateEventPage />} />
+            <Route path="/Editar/Attendants/Form" element={<EditAttendantsPage />} />
 
 
             <Route path="*" element={<NotFoundPage />} />
