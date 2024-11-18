3f2a2eb7d79d4a42ad1d2a36efd653461f23c4c2
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2024-11-18 11:02:17 +0100

    doing like button

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,12 +1,21 @@
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const EventCard = ({ cover, title, id }) => {
+const EventCard = ({ cover, title, id, liked }) => {
+    const [isLiked, setIsLiked] = useState(liked);
 
+    const handleLikedButton = () => {
+        setIsLiked = (!isLiked)
+    }
     return (
         <div className="EventCard">
             <Card className="bg-dark text-white">
                 <Card.Img src={cover} alt="Card image" />
+                <Button
+                    variant={isLiked ? "danger " : "secondary"}
+                    onClick={handleLikedButton}
+                >Danger</Button>
+
                 <Link to={`/eventos/detalles/${id}`} style={{ textDecoration: 'none' }}>
                     <Card.ImgOverlay className="d-flex justify-content-center align-items-start">
                         <Card.Title className="event-title text-center w-100">{title}</Card.Title >
