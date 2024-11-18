97dacf3e8bf3891e4d4983dbf940574e6315b9e4
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2024-11-18 20:56:20 +0100

    checkboxes doneee

--- src/components/EditAttendantsForm/EditAttendantsForm.jsx
+++ src/components/EditAttendantsForm/EditAttendantsForm.jsx
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Form } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { Form, Button, Col, Row } from "react-bootstrap";
 
-import { Button, Col, Row } from 'react-bootstrap';
 
 const API_URL = "http://localhost:5005";
 
@@ -54,99 +52,122 @@ const EditAttendantsForm = () => {
         setAttendantData({ ...attendantData, [name]: value });
     }
 
+    const hasFavouriteGenre = (genre) => {
+        return attendantData.favouriteMusicGenre.includes(genre);
+    }
+
+    const handleMusicGenreChange = (genre) => {
+        if (hasFavouriteGenre(genre)) {
+            const newFavouriteMusicGenre = attendantData.favouriteMusicGenre.filter(genre => genre !== genre);
+
+            const reqPayload = {
+                ...attendantData,
+                favouriteMusicGenre: newFavouriteMusicGenre
+            }
+
+            setAttendantData(reqPayload);
+        } else {
+            const reqPayload = {
+                ...attendantData,
+                favouriteMusicGenre: [...attendantData.favouriteMusicGenre, genre]
+            }
+
+            setAttendantData(reqPayload);
+
+
+        }
+    }
+
     return (
         <div className="EditAttendantsForm">
             {
                 isLoading ? <h1>CARGHANDO</h1> :
-                    <>
-                        <Form onSubmit={handleAttendantSubmit} >
-                            <Row className="mb-2">
+                    <Form onSubmit={handleAttendantSubmit} >
+                        <Row className="mb-2">
 
-                                <Form.Group as={Col} controlId="formGridName">
-                                    <Form.Label>Name</Form.Label>
-                                    <Form.Control type="text" value={attendantData.name} name="name" onChange={handleAttendantChange} ></Form.Control>
-                                </Form.Group>
+                            <Form.Group as={Col} controlId="formGridName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="text" value={attendantData.name} name="name" onChange={handleAttendantChange} />
+                            </Form.Group>
 
-                                <Form.Group as={Col} controlId="formGridLastName">
-                                    <Form.Label>LastName</Form.Label>
-                                    <Form.Control type="text" value={attendantData.lastName} name="lastName" onChange={handleAttendantChange} ></Form.Control>
-                                </Form.Group>
-
+                            <Form.Group as={Col} controlId="formGridLastName">
+                                <Form.Label>LastName</Form.Label>
+                                <Form.Control type="text" value={attendantData.lastName} name="lastName" onChange={handleAttendantChange} />
+                            </Form.Group>
 
 
-                            </Row>
 
-                            <Row>
-                                <Form.Group as={Col} controlId="formGridName">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control type="text" value={attendantData.email} name="email" onChange={handleAttendantChange} ></Form.Control>
-                                </Form.Group>
+                        </Row>
 
-                                <Form.Group as={Col} controlId="formGridName">
-                                    <Form.Label>Fecha de nacimiento</Form.Label>
-                                    <Form.Control type="text" value={attendantData.birth} name="birth" onChange={handleAttendantChange}></Form.Control>
-                                </Form.Group>
+                        <Row>
+                            <Form.Group as={Col} controlId="formGridName">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="text" value={attendantData.email} name="email" onChange={handleAttendantChange} />
+                            </Form.Group>
 
-                                <Form.Group as={Col} controlId="formGridName">
-                                    <Form.Label>Número de telefono</Form.Label>
-                                    <Form.Control type="number" value={attendantData.phone} name="phone" onChange={handleAttendantChange}></Form.Control>
-                                </Form.Group>
-                            </Row>
-                            <Row className="mb-3">
-                                <Form.Group as={Col} xs={6} controlId="formGridState">
-                                    <Form.Label>Género</Form.Label>
-                                    <Form.Select
-                                        placeholder="Género"
-                                        value={attendantData.gender}
+                            <Form.Group as={Col} controlId="formGridName">
+                                <Form.Label>Fecha de nacimiento</Form.Label>
+                                <Form.Control type="text" value={attendantData.birth} name="birth" onChange={handleAttendantChange} />
+                            </Form.Group>
 
-                                        name={'gender'}
-                                        onChange={handleAttendantChange}
-                                    >
-                                        <option value="">Seleccionar género</option>
-                                        <option value="Mujer">Mujer</option>
-                                        <option value="Hombre">Hombre</option>
-                                    </Form.Select>
-                                </Form.Group>
-                            </Row>
-                            <Row>
-                                <Form.Group as={Col} xs={6} id="formGridCheckbox" className="d-flex align-items-center mt-3">
-                                    <Form.Check type="checkbox" label="hardcore" />
+                            <Form.Group as={Col} controlId="formGridName">
+                                <Form.Label>Número de telefono</Form.Label>
+                                <Form.Control type="number" value={attendantData.phone} name="phone" onChange={handleAttendantChange} />
+                            </Form.Group>
+                        </Row>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} xs={6} controlId="formGridState">
+                                <Form.Label>Género</Form.Label>
+                                <Form.Select
+                                    placeholder="Género"
+                                    value={attendantData.gender}
 
-                                </Form.Group>
-                                <Form.Group as={Col} xs={6} id="formGridCheckbox" className="d-flex align-items-center mt-3">
-
-                                    <Form.Check type="checkbox" label="techno house" />
-                                </Form.Group>
-
-                                {
-                                    isLoading ? <h1>CARGANDO...</h1> :
-                                        attendantData && attendantData.favouriteMusicGenre && attendantData.favouriteMusicGenre.length > 0 ? (
-                                            <Row>
-                                                {attendantData.favouriteMusicGenre.map((genre, index) => (
-                                                    <Form.Group as={Col} key={index} xs={6} className="d-flex align-items-center mt-3">
-                                                        <Form.Check
-                                                            type="checkbox"
-                                                            label={genre}
-                                                            checked={true}
-                                                        />
-                                                    </Form.Group>
-                                                ))}
-                                            </Row>
-                                        ) : <p>No genres available</p>
-                                }
-                                <Form.Group as={Col} xs={6} id="formGridCheckbox" className="d-flex align-items-center mt-3">
-                                    <Form.Check type="checkbox" label="techno" />
-                                </Form.Group>
-                                <Form.Group as={Col} xs={6} id="formGridCheckbox" className="d-flex align-items-center mt-3">
-                                    <Form.Check type="checkbox" label="reggueton" />
-                                </Form.Group>
-                            </Row>
-                            <Col> <Button variant="dark" className="btn btn-outline-light AttendantsButton" type="submit">
-                                Actualizar perfil
-                            </Button>
-                            </Col>
-                        </Form>
-                    </>
+                                    name="gender"
+                                    onChange={handleAttendantChange}
+                                >
+                                    <option value="">Seleccionar género</option>
+                                    <option value="Mujer">Mujer</option>
+                                    <option value="Hombre">Hombre</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} xs={6} id="formGridCheckbox" className="d-flex align-items-center mt-3">
+                                <Form.Check
+                                    type="checkbox"
+                                    label="hardcore"
+                                    value="hardcore"
+                                    checked={() => hasFavouriteGenre("hardcore")}
+                                    onChange={() => handleMusicGenreChange("hardcore")}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="techno house"
+                                    value="techno house"
+                                    checked={hasFavouriteGenre("techno house")}
+                                    onChange={() => handleMusicGenreChange("techno house")}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="techno"
+                                    value="techno"
+                                    checked={hasFavouriteGenre("techno")}
+                                    onChange={() => handleMusicGenreChange("techno")}
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="reggueton"
+                                    value="reggueton"
+                                    checked={hasFavouriteGenre("reggueton")}
+                                    onChange={() => handleMusicGenreChange("reggueton")}
+                                />
+                            </Form.Group>
+                        </Row>
+                        <Col> <Button variant="dark" className="btn btn-outline-light AttendantsButton" type="submit">
+                            Actualizar perfil
+                        </Button>
+                        </Col>
+                    </Form>
             }
         </div>
     )
