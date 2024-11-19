a45f06c025ebc5acb5c955a8cde63e6eaa71854c
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2024-11-19 18:38:57 +0100

    login

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -3,8 +3,8 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import { AuthContext } from '../../contexts/auth.context';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth.context';
 
 const LogInForm = () => {
     const navigate = useNavigate();
